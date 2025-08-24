c6ad40d69dc26eb24e5d86e986e6c92397a494ab
YAMANI Yassine <yassine145287@gmail.com>
2025-08-24 15:38:05 +0200

    github pages final

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -39,7 +39,7 @@ const Hero = () => {
               <Button
                 variant="outline-secondary"
                 size="lg"
-                href="/portfolio/YAMANI-YASSINE-CV-2025.pdf.pdf"
+                href="/portfolio/YAMANI-YASSINE-CV-2025.pdf"
                 download
               >
                 Download CV <FaDownload className="ms-2" />
